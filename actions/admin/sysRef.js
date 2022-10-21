9436c96bfc6bf3ad4529c95cbbe7da241774477e
NMI <66474195+NMIFUN@users.noreply.github.com>
2022-10-21 10:06:08 +0500

    update ref-view and subs

--- actions/admin/sysRef.js
+++ actions/admin/sysRef.js
@@ -4,43 +4,72 @@ const User = require('../../models/user')
 const dateConfig = { year: 'numeric',month:'numeric',day:'numeric',hour:'numeric',minute: 'numeric'}
 const Markup = require('telegraf/markup')
 
-module.exports = async (ctx) => {
-  let shift = Number(ctx.state[0])
-  if(!shift) shift = 0
-  if(shift<0) return ctx.answerCbQuery('Нельзя', true)
+const defaultShift = 20
 
+module.exports = async (ctx) => {
+  if(!ctx.state[0]) ctx.state[0] = 0
+  
+  if(isNaN(ctx.state[0])) {
+    await ctx.answerCbQuery()
+
+    const [
+      result,
+      alive,
+    ] = await Promise.all([
+      Ref.findOne({ name: ctx.state[0] }),
+
+      User.countDocuments({ from: `ref-${ctx.state[0]}`, alive: true })
+    ])
+
+    return ctx.editMessageText(`
+Всего переходов: ${result.count} 
+Уникальных переходов: ${result.uniqueCount} (${Math.round(result.uniqueCount / result.count * 100)}%)
+Новых пользователей: ${result.newCount} (${Math.round(result.newCount / result.count * 100)}%)
+Живых пользователей: ${alive} (${Math.round(alive / result.count * 100)}%)
+
+Последний переход: ${new Date(result.last).toLocaleString('ru', dateConfig)}
+
+Ссылка: https://t.me/${process.env.BOT_USERNAME}?start=ref-${result.name}
+    `, Markup.inlineKeyboard([
+      [Markup.callbackButton(`🔄 Обновить`, `admin_sysRef_${result.name}`)],
+      [Markup.callbackButton(`Назад`, `admin_sysRef`)]
+    ]).extra({ 
+      parse_mode: 'HTML',
+      disable_web_page_preview: true
+    }))
+  }
+
+  let shift = Number(ctx.state[0])
   const count = await Ref.countDocuments()
-  if(shift >= count) return ctx.editMessageText(`Реферальных ссылок еще не существует.\n
+  if(shift < 0 || shift >= count) return ctx.answerCbQuery('Нельзя', true)
+  await ctx.answerCbQuery()
+
+  if(!count) return ctx.editMessageText(`Реферальных ссылок еще не существует.\n
 <code>https://t.me/${process.env.BOT_USERNAME}?start=ref-</code>code, переходя по такой ссылке пользователь автоматически учитывается в списке.
 code - любой код для отличия ссылки от других ссылок`, Markup.inlineKeyboard(
     [Markup.callbackButton(`Назад`, `admin_back`)]
-  ).extra({ parse_mode: 'HTML' }))
+  ).extra({ 
+    parse_mode: 'HTML',
+    disable_web_page_preview: true
+  }))
 
-  await ctx.answerCbQuery()
-
-  const results = await Ref.find().skip(shift).limit(10).sort({ _id: -1 })
+  const results = await Ref.find().skip(shift).limit(defaultShift).sort({ _id: -1 })
   
-  let content = results.map(async result => {
-    const alive = await User.countDocuments({ from: `ref-${result.name}`, alive: true })
-
-    return `<b>${result.name}</b>:
-Всего/Уникальных переходов: ${result.count} / ${result.uniqueCount}
-Новых/Живых пользователей: ${result.newCount} / ${alive}
-Последний переход: ${new Date(result.last).toLocaleString('ru', dateConfig)}`
-  })
-  content = await Promise.all(content)
+  let content = results.map(result => `<b>${result.name}</b>: ${result.count} / ${result.uniqueCount}`)
+  const keyboard = results.map(result => Markup.callbackButton(`${result.name} ${result.count}`, `admin_sysRef_${result.name}`))
 
   return ctx.editMessageText(`
-<code>https://t.me/${process.env.BOT_USERNAME}?start=ref-</code>code, переходя по такой ссылке пользователь автоматически учитывается в списке.
-code - любой код для отличия ссылки от других ссылок
+<code>https://t.me/${process.env.BOT_USERNAME}?start=ref-</code>code
 
-${content.join('\n\n')}`, {
+${content.join('\n')}`, {
     parse_mode: 'HTML',
-    reply_markup: Markup.inlineKeyboard([
-      [Markup.callbackButton(`◀️`, `admin_sysRef_${shift-10}`),
-      Markup.callbackButton(`${shift + results.length}/${count} 🔄`, `admin_sysRef_${shift}`),
-      Markup.callbackButton(`▶️`, `admin_sysRef_${shift+10}`)],
-      [Markup.callbackButton(`Назад`, `admin_back`)]
-    ])
+    reply_markup: { 
+      inline_keyboard: Markup.inlineKeyboard(keyboard, { columns: 2 }).inline_keyboard.concat([
+        [Markup.callbackButton(`◀️`, `admin_sysRef_${shift - defaultShift}`),
+        Markup.callbackButton(`${shift + results.length}/${count} 🔄`, `admin_sysRef_${shift}`),
+        Markup.callbackButton(`▶️`, `admin_sysRef_${shift + defaultShift}`)],
+        [Markup.callbackButton(`Назад`, `admin_back`)]
+      ])
+    }
   })
 }
--- middlewares/subscription.js
+++ middlewares/subscription.js
@@ -8,8 +8,9 @@ const { Telegraf } = require('telegraf')
 module.exports = async (ctx, next) => {
   if (config.admins.includes(ctx.from.id) || ctx?.chat?.type !== 'private') return next()
 
+  let notSubscribed = []
   const channels = config.subsChannels?.filter(channel => [undefined, ctx.user?.lang, 'all'].includes(channel.lang))
-  let notSubscribed = await asyncFilter(channels, async channel => {
+  if(channels?.length) notSubscribed = await asyncFilter(channels, async channel => {
     const check = await ctx.telegram.getChatMember(channel.id, ctx.from.id).catch(() => {}) || {}
 
     if(['left', 'kicked'].includes(check.status)) return true
@@ -17,7 +18,7 @@ module.exports = async (ctx, next) => {
   })
 
   const bots = config.subsBots?.filter(channel => [undefined, ctx.user?.lang, 'all'].includes(channel.lang))
-  notSubscribed = notSubscribed.concat(await asyncFilter(bots, async bot => {
+  if(bots?.length) notSubscribed = notSubscribed.concat(await asyncFilter(bots, async bot => {
     const connectedBot = new Telegraf(bot.token)
     const check = await connectedBot.telegram.sendChatAction(ctx.from.id, 'typing').catch(() => {}) || false
 
@@ -27,14 +28,11 @@ module.exports = async (ctx, next) => {
   if(notSubscribed.length) {
     if(ctx.callbackQuery) await ctx.answerCbQuery(ctx.i18n.t('subscribe.notif'))
 
-    let count = 0
-
     return ctx[ctx.updateType === 'message' ? 'replyWithHTML' : 'editMessageText'](ctx.i18n.t('subscribe.text'), {
       parse_mode: 'HTML',
       reply_markup: Markup.inlineKeyboard([
-        ...notSubscribed.map(channel => {
-          count++
-          return Markup.urlButton(`${ctx.i18n.t(`subscribe.${channel.token ? 'bot' : 'channel'}`)} №${count}`, channel.link)
+        ...notSubscribed.map((channel, index) => {
+          return Markup.urlButton(`${ctx.i18n.t(`subscribe.${channel.token ? 'bot' : 'channel'}`)} №${index + 1}`, channel.link)
         }),
         Markup.callbackButton(ctx.i18n.t('subscribe.key.check'), `subscription`)
       ], { columns: 1 }),
