8123bb3b3449303285b7733f8eaaf653356a1317
NMI <66474195+NMIFUN@users.noreply.github.com>
2022-11-21 13:52:23 +0500

    update listUsers and sysRefs

--- actions/admin/listUsers.js
+++ actions/admin/listUsers.js
@@ -8,32 +8,33 @@ module.exports = async (ctx) => {
   if(ctx.state[0]) {
     await ctx.answerCbQuery(`Экспортирование`, true)
 
+    let contentUsers = []
     switch (ctx.state[0]) {
       case 'alive':
         var users = await User.find({ alive: true }, '-_id id').lean()
 
-        var content = users.map((value) => Object.values(value).join(';')).join('\n')
+        contentUsers = users.map((value) => Object.values(value))
         break
       case 'all':
         var users = await User.find({}, '-_id id').lean()
 
-        var content = users.map((value) => Object.values(value).join(';')).join('\n')
+        contentUsers = users.map((value) => Object.values(value))
         break
       case 'full':
-        var users = await User.find({},  Object.keys(exportTemplate).join(' ')).lean()
+        var users = await User.find({}, Object.keys(exportTemplate).join(' ')).lean()
 
-        var content = Object.keys(exportTemplate).join(';')
-        content += `\n${users.map((value) => Object.values({ ...exportTemplate, ...value }).join(';')).join('\n')}`
+        contentUsers = [Object.keys(exportTemplate).join(';')]
+        contentUsers.push(users.map((value) => Object.values({ ...exportTemplate, ...value }).join(';')))
         break
     }
 
-    return ctx.replyWithDocument({ source: Buffer.from(content, 'utf8'), filename: `users.csv` })
+    return ctx.replyWithDocument({ source: Buffer.from(contentUsers.join('\n'), 'utf8'), filename: `users.csv` })
   } else {
     await ctx.answerCbQuery()
 
     return ctx.editMessageText(`Выберите вариант экспорта:`, Markup.inlineKeyboard([
       [
-        Markup.callbackButton(`Бекап`, `admin_listUsers_full`),
+        //Markup.callbackButton(`Бекап`, `admin_listUsers_full`),
         Markup.callbackButton(`Полный`, `admin_listUsers_all`),
         Markup.callbackButton(`Живые`, `admin_listUsers_alive`),
       ], 
--- middlewares/sysRefs.js
+++ middlewares/sysRefs.js
@@ -8,19 +8,17 @@ module.exports = async (ctx, next) => {
   
   const cmd = (split[1] && split[1].split('-') || [])
 
-  if (cmd[0] !== 'ref') return next()
+  if (cmd[0] === 'ref') {
+    let newCount = 0
+    if(ctx.freshUser) newCount = 1
 
-  let newCount = 0
-  if(ctx.freshUser) newCount = 1
-
-  const date = Date.now()
-  const find = await Ref.findOne({ name: cmd[1] })
-  if(find) {
-    const findIclude = find.users.includes(ctx.from.id)
-    if(findIclude) await Ref.updateOne({ name: cmd[1] }, { $inc: { count: 1, newCount: newCount }, $set: { last: date }})
-    else await Ref.updateOne({ name: cmd[1] }, { $inc: { count: 1, newCount: newCount, uniqueCount: 1}, $push: { users: ctx.from.id }, $set: { last: date }})
-  }else{
-    await Ref.create({
+    const date = Date.now()
+    const find = await Ref.findOne({ name: cmd[1] })
+    if(find) {
+      const findIclude = find.users.includes(ctx.from.id)
+      if(findIclude) await Ref.updateOne({ name: cmd[1] }, { $inc: { count: 1, newCount: newCount }, $set: { last: date }})
+      else await Ref.updateOne({ name: cmd[1] }, { $inc: { count: 1, newCount: newCount, uniqueCount: 1}, $push: { users: ctx.from.id }, $set: { last: date }})
+    } else await Ref.create({
       name: cmd[1],
       first: date,
       last: date,
